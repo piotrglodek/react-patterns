15f8b4737909aa37e96fedb808d04329c229cbba
Piotr Głodek <pioglodek@gmail.com>
2021-11-24 18:29:19 +0100

    Create App and custom Link.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { Outlet } from 'react-router-dom';
+import './sass/components/_app.sass';
+import { Link } from './components/Link';
+
 function App() {
   return (
-    <>
-      <h1>React Patterns</h1>
-    </>
+    <div className='app'>
+      <h1 className='app__heading'>React Patterns</h1>
+      <nav className='app__nav'>
+        <Link className='app__link' activeClassName='app__link--active' to='/'>
+          Patterns
+        </Link>
+        <Link
+          className='app__link'
+          activeClassName='app__link--active'
+          to='container-presentional'
+        >
+          Container and Presentional
+        </Link>
+      </nav>
+      <div className='app__content'>
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
