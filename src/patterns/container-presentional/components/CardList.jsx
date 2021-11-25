af6f6f8322d3fc8b084dd1033be2216431bb25fd
Piotr Głodek <pioglodek@gmail.com>
2021-11-25 22:36:42 +0100

    Create fetch more functionality.

--- src/patterns/container-presentional/components/CardList.jsx
+++ src/patterns/container-presentional/components/CardList.jsx
@@ -2,17 +2,31 @@ import PropTypes from 'prop-types';
 import Card from './Card';
 import '../../../sass/components/_cardList.sass';
 
-const CardList = ({ data }) => (
-  <div className='card-list'>
-    {data.results.map(character => (
-      <Card
-        key={character.id}
-        name={character.name}
-        image={character.image}
-        status={character.status}
-      />
-    ))}
-  </div>
+import { Button } from '../../../components/Button';
+import { Spinner } from '../../../components/Spinner';
+
+const CardList = ({ data, hasMore, isFetchingMore, handleFetchMore }) => (
+  <>
+    <div className='card-list'>
+      {data.results.map(character => (
+        <Card
+          key={character.id}
+          name={character.name}
+          image={character.image}
+          status={character.status}
+        />
+      ))}
+    </div>
+    <div className='card-list-container'>
+      {isFetchingMore ? (
+        <Spinner />
+      ) : hasMore ? (
+        <Button disabled={isFetchingMore} onClick={handleFetchMore}>
+          Fetch more
+        </Button>
+      ) : null}
+    </div>
+  </>
 );
 
 const baseShape = PropTypes.shape({
--- src/patterns/container-presentional/container/RickAndMortyCharacters.jsx
+++ src/patterns/container-presentional/container/RickAndMortyCharacters.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import CardList from '../components/CardList';
 import { Spinner } from '../../../components/Spinner';
 // hook
@@ -7,6 +9,41 @@ const API_URL = `https://rickandmortyapi.com/api/character`;
 
 export const RickAndMortyCharacters = () => {
   const [data, isLoading, error] = useFetch(API_URL);
+  const [_data, _setData] = useState(null);
+  const [isFetchingMoreData, setIsFetchingMoreData] = useState(false);
+  const [nextData, setNextData] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      _setData(data);
+      setNextData(data.info.next);
+    }
+  }, [data]);
+
+  const fetchMore = async () => {
+    try {
+      setIsFetchingMoreData(true);
+      const response = await fetch(nextData);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error('😱 Something went wrong with fetching data!');
+      }
+      setNextData(data.info.next);
+      _setData(prev => {
+        return {
+          ...prev,
+          info: {
+            ...prev.info,
+            next: data.info.next,
+          },
+          results: [...prev.results, ...data.results],
+        };
+      });
+      setIsFetchingMoreData(false);
+    } catch (error) {
+      setIsFetchingMoreData(false);
+    }
+  };
 
   if (error) {
     return <p>{error}</p>;
@@ -16,5 +53,12 @@ export const RickAndMortyCharacters = () => {
     return <Spinner />;
   }
 
-  return <CardList data={data} />;
+  return (
+    <CardList
+      data={_data}
+      hasMore={nextData}
+      isFetchingMore={isFetchingMoreData}
+      handleFetchMore={fetchMore}
+    />
+  );
 };
